8f8be1ae06ac96b6d25aa456e5f7d9f21b7e9adc
Maykoll <maycol.gonzalez7@gmail.com>
2015-09-11 12:00:07 -0400

    Update cola.h

--- cola.h
+++ cola.h
@@ -1,1 +1,18 @@
+#ifndef stdio_h
+#include <stdio.h>
+#define stdio_h
+#endif
+
+#ifndef stdlib_h
+#include <stdlib.h>
+#define stdio_h
+#endif
+
+//#define CAPACIDAD 8
+
+typedef  struct tipo_cola{ 
+    int tamaño;
+    int fin;
+    int *elementos;
+} Cola;
 
