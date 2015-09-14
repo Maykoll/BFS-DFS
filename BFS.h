ad6637d3de0b724d24d35034ab7d8dd2c551bfdc
Maykoll <maycol.gonzalez7@gmail.com>
2015-09-13 23:25:44 -0400

    Update BFS.h

--- BFS.h
+++ BFS.h
@@ -1,1 +1,32 @@
+// deveria ser algo asi pero ya no alcanso a terminar nada 
 
+BFS(G,s) 
+ {  
+  
+   int d[N], p[N], color[N]; 
+   Cola Q;	
+   for ( cada vértice u ? V[G] -{s}) 
+   {
+     color [u] =Blanco;		
+     d[u] = -1;  // deveria ser infinito pero nose como ponerlo o seria...
+   }	
+      color[s] =gris;
+      d[s] = 0; 
+      p [s]=NULL;
+      llegada(Q, s);  // se agrega s ala cola 
+   while ( !esVacia(Q) ) 
+   {
+      u = Cabeza(Q);   
+      for ( for i=0; i<adyacentes[u]; i++ )
+      {
+        if (color [v] == Blanco) 
+        {			
+          color[v]=Plomo;  			
+          d [i]=d [i] +1;			
+          p [i] = u;		
+          llegada(Q,i);		
+        }	
+          color [i] = Negro;  
+      }
+   }
+  }  
